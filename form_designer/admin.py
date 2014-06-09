8ba3d44bc3f671fb3969ffee65f289c8d33e1350
Matthew Ghantous <matthew@againfaster.com>
2014-06-09 17:19:18 -0400

    AFW-398 AFW-401 #resolve
    Merge branch 'master' of https://github.com/samluescher/django-form-designer
    
    Conflicts:
    	form_designer/admin.py
    	form_designer/migrations/0008_auto__add_formvalue__del_field_formlog_data__add_field_formlog_created.py
    
    Also made a custom form template that shows messages.

--- form_designer/admin.py
+++ form_designer/admin.py
@@ -90,11 +90,10 @@ class FormLogAdmin(admin.ModelAdmin):
         """
         list_display = self.get_list_display(request)
         list_display_links = self.get_list_display_links(request, list_display)
-        list_filter = self.get_list_filter(request)
         ChangeList = self.get_changelist(request)
 
         cl = ChangeList(request, self.model, list_display,
-            list_display_links, list_filter, self.date_hierarchy,
+            list_display_links, self.list_filter, self.date_hierarchy,
             self.search_fields, self.list_select_related,
             self.list_per_page, self.list_max_show_all, self.list_editable,
             self)
